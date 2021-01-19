8ad4bb263b9258bc58803ffae71b5e8c329cff0e
jayeom-fitz <djawjdgus93@naver.com>
2021-01-19 20:27:01 +0900

    #0-2 Installs

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeEnIwSHk8w5AzM6EctCpQOQhwM6lmpno",
+  authDomain: "instagram-dev-76486.firebaseapp.com",
+  projectId: "instagram-dev-76486",
+  storageBucket: "instagram-dev-76486.appspot.com",
+  messagingSenderId: "417165643284",
+  appId: "1:417165643284:web:92676d6eb58e5063c76101",
+  measurementId: "G-G9W3LHZPST"
+};
+
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 import { NavigationContainer } from '@react-navigation/native';
-import { creacteStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
 
-const stack = creacteStackNavigator();
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator InitialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown : false }} />
+        <Stack.Screen name="Landing" 
+                      component={LandingScreen} 
+                      options={{ headerShown : false }} />
+        <Stack.Screen name="Register" 
+                      component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer> 
   );

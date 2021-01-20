646c6bf0357a062089c1282a3413c4a202f2228d
jayeom-fitz <djawjdgus93@naver.com>
2021-01-20 23:39:48 +0900

    #0-6 Redux and Main

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native';
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchUser } from '../redux/actions/index'
+
 export class Main extends Component {
   componentDidMount() {
-
+    this.props.fetchUser();
   }
 
   render() {
+    const { currentUser } = this.props;
+    
+    console.log(currentUser);
+    if(currentUser == undefined) {
+      return (
+        <View>none</View>
+      );
+    }
+
+
     return (
       <View style={{ flex : 1, justifyContent : 'center' }}>
         <Text>User Logged In</Text>
@@ -15,4 +29,9 @@ export class Main extends Component {
   }
 }
 
-export default Main
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser
+});
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchProps)(Main)
